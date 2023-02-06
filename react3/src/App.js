6a1099d66a05bd12aa4ddfcb2cc94fabf91568b5
55973 <55973@etu.he2b.be>
2023-02-06 21:01:01 +0100

    tailwinds fini

---
+++ react3/src/App.js
@@ -0,0 +1,14 @@
+ 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <h1 className="text-3xl text-green-400 text-center mt-10 font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
